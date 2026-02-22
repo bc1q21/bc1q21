e245bf6d1c6686fe2205ed64bff2d7b4260fe275
bc1q21 <info@bc1q21.com>
2026-02-22 16:12:21 -0600

    Add projects.js with sample project data

---
+++ Website/docs/projects.js
@@ -0,0 +1,10 @@
+const projects = [
+  {
+    title: "Sample Project",
+    description: "This is where your project description will appear.",
+    goal: 0.5,
+    raised: 0.1,
+    address: "bc1qexampleaddressxxxxxxxxxxxxxxxxxxxxxx",
+    image: "../images/project-placeholder.png"
+  }
+];
